a3b3cfac409f8d219435f370f675ed4cb2361d06
Yash Chowdhury <yashchowdhurysn@gmail.com>
2023-09-29 06:13:44 +0530

    changes 2.0

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
             <LeftPanel />
           </div>
         </div>
-        <div className='flex h-full w-2/5 justify-center mx-14'>
-          <div className='mt-2 w-full shadow-2xl rounded-lg shadow-purple-800'>
+        <div className='flex h-full w-2/4 justify-center'>
+          <div className='mt-2 w-3/4 shadow-2xl rounded-lg shadow-purple-800'>
             <Chat />
           </div>
         </div>
-        <div className='flex flex-grow h-full w-1/4 justify-center mr-3'>
-          <div className='mt-2 mx-3 w-full'>
+        <div className='flex flex-grow h-full w-1/4 justify-center'>
+          <div className='mt-2 mx-3 mr-8 w-full'>
             <UserStreams />
           </div>
         </div>
